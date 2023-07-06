19eec3c43a76be23e14d7ea4aa4bc40264c4e0a2
Renan <renan.fachin@hotmail.com>
2023-07-06 19:03:17 -0300

    feat: auth with google account

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 'use client'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession, signOut } from 'next-auth/react'
 
 export default function Home() {
+  const session = useSession()
+
+
   return (
     <div>
       <button onClick={() => signIn()}>
         Login
+      </button>
+
+      <h1>Olá, {session.data?.user?.name}</h1>
+
+      <button onClick={() => signOut()}>
+        Sair
       </button>
     </div>
   )
