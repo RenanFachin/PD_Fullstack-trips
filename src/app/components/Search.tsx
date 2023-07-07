66f23025ae9157b3c45071f674abd3290f7aaae7
Renan <renan.fachin@hotmail.com>
2023-07-07 14:02:11 -0300

    feat: add initial search component

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,12 +1,13 @@
 'use-client'
 
+import { Button } from "@/components/Button";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import DatePicker from "@/components/DatePicker";
 import Input from "@/components/Input";
 
 export function Search() {
   return (
-    <section className="container mx-auto p-2 mt-4">
+    <section className="container mx-auto p-2 mt-4 bg-search-background bg-cover bg-no-repeat bg-center">
       <h2
         className="font-semibold text-2xl text-secondary text-center">
         Encontre sua próxima <span className="text-primary">viagem</span>!
@@ -19,6 +20,10 @@ export function Search() {
           <DatePicker placeholderText="Data de ida" onChange={() => { }} className="w-full" />
           <CurrencyInput placeholder="Orçamento" />
         </div>
+
+        <Button>
+          Buscar
+        </Button>
       </div>
 
     </section>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function Button({ className, ...props }: ComponentPropsWithoutRef<"button">) {
   const _className = twMerge(
     className,
-    "appearance-none rounded-lg bg-primary p-2 text-sm font-medium text-white shadow transition-all hover:bg-primaryDarker"
+    "appearance-none rounded-lg bg-primary p-2 text-sm font-medium text-white shadow transition-all hover:bg-secondary"
   );
 
   return (
