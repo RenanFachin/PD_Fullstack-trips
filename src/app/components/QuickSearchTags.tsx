d9bb468ae036c863d4b394ae4c4a56261595e342
Renan <renan.fachin@hotmail.com>
2023-07-08 19:38:51 -0300

    feat: implements quick search

--- src/app/components/QuickSearchTags.tsx
+++ src/app/components/QuickSearchTags.tsx
@@ -5,6 +5,7 @@ import HotelImage from '../../../public/hotel-icon.png'
 import CottageImage from '../../../public/cottage-icon.png'
 import FarmImage from '../../../public/farm-icon.png'
 import InnImage from '../../../public/inn-icon.png'
+import Link from "next/link";
 
 export function QuickSearchTags() {
   return (
@@ -19,25 +20,32 @@ export function QuickSearchTags() {
 
       <div className="flex w-full justify-between mt-4">
         <div className="flex flex-col items-center justify-center gap-1">
-          <Image width={32} height={32} src={HotelImage} alt="Hotel" />
-
+          <Link href={'/trips/search?text=hotel'}>
+            <Image width={32} height={32} src={HotelImage} alt="Hotel" />
+          </Link>
           <span className="text-sm text-gray-100">Hotel</span>
         </div>
 
         <div className="flex flex-col items-center justify-center gap-1">
-          <Image width={32} height={32} src={CottageImage} alt="Hotel" />
+          <Link href={'/trips/search?text=chale'}>
+            <Image width={32} height={32} src={CottageImage} alt="Chalé" />
+          </Link>
 
           <span className="text-sm text-gray-100">Chalé</span>
         </div>
 
         <div className="flex flex-col items-center justify-center gap-1">
-          <Image width={32} height={32} src={InnImage} alt="Hotel" />
+          <Link href={'/trips/search?text=pousada'}>
+            <Image width={32} height={32} src={InnImage} alt="Pousada" />
+          </Link>
 
           <span className="text-sm text-gray-100">Pousada</span>
         </div>
 
         <div className="flex flex-col items-center justify-center gap-1">
-          <Image width={32} height={32} src={FarmImage} alt="Hotel" />
+          <Link href={'/trips/search?text=fazenda'}>
+            <Image width={32} height={32} src={FarmImage} alt="Fazenda" />
+          </Link>
 
           <span className="text-sm text-gray-100">Fazenda</span>
         </div>
